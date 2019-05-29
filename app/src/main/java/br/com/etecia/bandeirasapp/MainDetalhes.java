cae1d7503f483e3f59e57ff6625b2bbffd7ede7d
= <=>
2019-05-28 22:29:40 -0300

    upload project

---
+++ app/src/main/java/br/com/etecia/bandeirasapp/MainDetalhes.java
@@ -0,0 +1,13 @@
+package br.com.etecia.bandeirasapp;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.etecia.bandeirasapp.Model.State;
+public class MainDetalhes extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+
+}
